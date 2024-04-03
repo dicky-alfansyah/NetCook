3b3fc191986581dc60dac3460eb63420b1ef03f6
dicky-alfansyah <dickyalfansyaah@gmail.com>
2024-04-04 01:21:33 +0700

    Update app.py

--- api/app.py
+++ api/app.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-TEMP_FOLDER = os.path.join(app.root_path, 'api', 'temp')
+TEMP_FILES = {}
+
 
 def generate_unique_id():
     return str(uuid.uuid4())
@@ -24,19 +25,13 @@ def generate_unique_id():
 def create_temp_directory():
     if 'temp_id' not in session:
         session['temp_id'] = generate_unique_id()
-    temp_dir = get_temp_directory(session['temp_id'])
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
+    if session['temp_id'] not in TEMP_FILES:
+        TEMP_FILES[session['temp_id']] = {}
 
 
 def delete_temp_directory(temp_id):
-    temp_dir = get_temp_directory(temp_id)
-    if os.path.exists(temp_dir):
-        shutil.rmtree(temp_dir)
-
-
-def get_temp_directory(temp_id):
-    return os.path.join(TEMP_FOLDER, temp_id)
+    if temp_id in TEMP_FILES:
+        del TEMP_FILES[temp_id]
 
 
 def check_cookies_valid_netflix(cookies):
@@ -77,9 +72,11 @@ def process_uploaded_file(file_path, temp_id):
 
 def extract_and_process_cookies_zip(file_path, temp_id):
     result_df = pd.DataFrame(columns=['File Name', 'Cookies Status'])
-    temp_folder = get_temp_directory(temp_id)
+    temp_files = TEMP_FILES[temp_id]
 
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        temp_folder = os.path.join(
+            app.root_path, 'temp', session['temp_id'])
         zip_ref.extractall(temp_folder)
 
     extracted_files = [f for f in os.listdir(
@@ -108,9 +105,8 @@ def extract_and_process_cookies_single(file_path, temp_id):
 
 
 def delete_temp_directory_after_delay(temp_id, delay):
-    while True:
-        time.sleep(delay)
-        delete_temp_directory(temp_id)
+    time.sleep(delay)
+    delete_temp_directory(temp_id)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -129,8 +125,8 @@ def upload_file():
             return render_template('index.html', error='Invalid file format!')
 
         create_temp_directory()
-        temp_dir = get_temp_directory(session['temp_id'])
-        delete_temp_directory(session['temp_id'])
+        temp_dir = os.path.join(
+            app.root_path, 'temp', session['temp_id'])
         if not os.path.exists(temp_dir):
             os.makedirs(temp_dir)
         file_path = os.path.join(temp_dir, file.filename)
@@ -205,7 +201,8 @@ def upload_file():
 
 @app.route('/download/<file_name>', methods=['GET'])
 def download_file(file_name):
-    temp_dir = get_temp_directory(session['temp_id'])
+    temp_dir = os.path.join(
+        app.root_path, 'temp', session['temp_id'])
     file_path = os.path.join(temp_dir, file_name)
     if os.path.exists(file_path):
         return send_file(file_path, as_attachment=True)
